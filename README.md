1681d534a5f92ca0e563a46ef0ae5704fb75f3d9
bora.tashan <bora.tashan@gmail.com>
2023-12-12 14:06:15 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## AllSaints SFCC image issue fixing tool
 
-## How to build the app
+## Dependencies 
 1)Install Java 17, https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/downloads-list.html
 
-2)Install Gradle https://gradle.org/install/
+
+## How to build
+2)run ./gradlew clean build installDist
+
--- src/test/java/allsaints/sfcc/worker/AllsaintsSfccWorkerCommandTest.java
+++ src/test/java/allsaints/sfcc/worker/AllsaintsSfccWorkerCommandTest.java
@@ -12,17 +12,4 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AllsaintsSfccWorkerCommandTest {
 
-    @Test
-    public void testWithCommandLineOption() throws Exception {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baos));
-
-        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
-            String[] args = new String[] { "-v" };
-            PicocliRunner.run(AllsaintsSfccWorkerCommand.class, ctx, args);
-
-            // allsaints-sfcc-worker
-            assertTrue(baos.toString().contains("Hi!"));
-        }
-    }
 }
